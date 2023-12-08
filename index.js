48cba2cfca6cc9a01918afdf0c1c23d67be1c9ba
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-08 21:14:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("The console is operating now.");
+
+let a;
+a = 5;
+console.log(a);
+a = a + 5;
+congole.log(a);
+let b;
+b = "Embracement";
+console.log(a, b);
+let c;
+c = false;
+console.log(c)
