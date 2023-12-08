6310b938023968ed2dd0c43a21797dd891333fa4
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-08 21:25:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ let a;
 a = 5;
 console.log(a);
 a = a + 5;
-congole.log(a);
+console.log(a);
 let b;
 b = "Embracement";
 console.log(a, b);
 let c;
 c = false;
 console.log(c)
+
+document.getelementbyid("p1").innerHTML = "This is the script text."
