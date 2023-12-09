7848f176e945024f9a3cbba1b70955dd723d6d25
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-09 12:08:01 +0100

    update

---
+++ ch0.js
@@ -0,0 +1,34 @@
+console.log("The console is operating now.");
+
+let a;
+a = 5;
+console.log(a);
+a = a + 5;
+console.log(a);
+let b;
+b = "Embracement";
+console.log(a, b);
+let c;
+c = false;
+console.log(c);
+
+document.getElementById("p1").innerHTML = "This is the script text.";
+
+let accessname = window.prompt("Access password:");
+if (accessname = "admin"){
+  window.alert("Access granted!");
+}
+else{
+  window.alert("Access denied, but I'll still let you in :)");
+}
+
+let someprompt;
+document.getElementById("SomeButton").onclick = function() {
+  someprompt = document.getElementById("p1").value;
+  window.alert("So what... you just said '" + "'.");
+  console.log("said: " + someprompt);
+  document.getElementbyId("p1").innerHTML = someprompt;
+}
+
+let textvolatile;
+setTimeout(5,function(){textvolatile = document.getElementById("VolatileText").innerHTML = "I swear, there was some change!"}); 
--- index.js
+++
@@ -1,6 +0,0 @@
-let counter = 0;
-counter = Number(0);
-document.getElementById("CounterButton1").onclick = function(){
-    counter++;
-    document.getElementById("CounterLabel").innerHTML = counter;
-}
