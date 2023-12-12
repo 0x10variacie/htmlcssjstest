66586aeb406b6677de6cc4a18ff5fa92f7d9d7d9
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-12 16:12:31 +0100

    upd

--- ch4.js
+++ ch4.js
@@ -6,26 +6,26 @@ let resval;
 
 document.getElementById("PlusButton").onclick = function() {
     capture();
-    resval = Number(firstval + secval);
+    var resval = Number(firstval + secval);
     document.getElementById("ResultValue").innerHTML = resval;
 };
 
 document.getElementById("MinusButton").onclick = function() {
     capture();
-    resval = Number(firstval - secval);
+    var resval = Number(firstval - secval);
     document.getElementById("ResultValue").innerHTML = resval;
 };
 
 document.getElementById("MultiButton").onclick = function() {
     capture();
-    resval = Number(firstval * secval);
+    var resval = Number(firstval * secval);
     document.getElementById("ResultValue").innerHTML = resval;
 };
 
 document.getElementById("DivideButton").onclick = function() {
     capture();
     if (secval !== 0) {
-        resval = Number(firstval / secval);
+        var resval = Number(firstval / secval);
         document.getElementById("ResultValue").innerHTML = resval;
     } else {
         window.alert("I cannot divide by zero!");
@@ -34,8 +34,8 @@ document.getElementById("DivideButton").onclick = function() {
 
 function capture() {
     firstval = document.getElementById("FirstValue").value;
-    firstval = Number(firstval);
+    var firstval = Number(firstval);
     secval = document.getElementById("SecValue").value;
-    secval = Number(secval);
+    var secval = Number(secval);
     console.log("Captured: ", firstval, ",", secval);
 }
