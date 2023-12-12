a012499f328efbaca5fe68c1b0127980c485a738
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-12 15:44:11 +0100

    upd

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstval;
+let secval;
+let resval;
+
+document.getElementById("PlusButton").onclick = function() {
+    var resval = firstval + secval;
+    var resval = document.getElementById(ResultValue).innerHTML = str(resval);
+}
+
+document.getElementById("MinusButton").onclick = function() {
+    var resval = firstval - secval;
+    var resval = document.getElementById(ResultValue).innerHTML = str(resval);
+}
+
+document.getElementById("MultiButton").onclick = function() {
+    var resval = firstval * secval;
+    var resval = document.getElementById(ResultValue).innerHTML = str(resval);
+}
+
+document.getElementById("DivideButton").onclick = function() {
+    if (secval != 0) {
+        var resval = firstval / secval;
+        var resval = document.getElementById(ResultValue).innerHTML = str(resval);} else {window.alert("I cannot divide by zero!")}
+}
