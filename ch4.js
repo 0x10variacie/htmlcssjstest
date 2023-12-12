50a31dfd96d87d3bebf31f0c1657152de37b66a3
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-12 16:01:27 +0100

    upd

--- ch4.js
+++ ch4.js
@@ -3,33 +3,34 @@ let firstval;
 let secval;
 let resval;
 
-function capture(firstval,secval) {
+function capture() {
     var firstval = document.getElementById("FirstValue").value
     firstval = Number(firstval)
     var secval = document.getElementById("SecValue").value
     firstval = Number(secval)
+    console.log("Captured: ",firstval,",",secval)
 }
 
 document.getElementById("PlusButton").onclick = function() {
-    capture(firstval,secval)
+    capture()
     var resval = Number(firstval + secval);
     var resval = document.getElementById("ResultValue").innerHTML = resval;
 }
 
 document.getElementById("MinusButton").onclick = function() {
-    capture(firstval,secval)
+    capture()
     var resval = Number(firstval - secval);
     var resval = document.getElementById("ResultValue").innerHTML = resval;
 }
 
 document.getElementById("MultiButton").onclick = function() {
-    capture(firstval,secval)
+    capture()
     var resval = Number(firstval * secval);
     var resval = document.getElementById("ResultValue").innerHTML = resval;
 }
 
 document.getElementById("DivideButton").onclick = function() {
-    capture(firstval,secval)
+    capture()
     if (secval != 0) {
         var resval = Number(firstval / secval);
         var resval = document.getElementById("ResultValue").innerHTML = resval;} else {window.alert("I cannot divide by zero!")}
