04cf3bbe32407732558e8481efa3517af06d7d96
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-12 15:49:09 +0100

    "ResultValue" is now in quotes

--- ch4.js
+++ ch4.js
@@ -5,21 +5,21 @@ let resval;
 
 document.getElementById("PlusButton").onclick = function() {
     var resval = firstval + secval;
-    var resval = document.getElementById(ResultValue).innerHTML = str(resval);
+    var resval = document.getElementById("ResultValue").innerHTML = str(resval);
 }
 
 document.getElementById("MinusButton").onclick = function() {
     var resval = firstval - secval;
-    var resval = document.getElementById(ResultValue).innerHTML = str(resval);
+    var resval = document.getElementById("ResultValue").innerHTML = str(resval);
 }
 
 document.getElementById("MultiButton").onclick = function() {
     var resval = firstval * secval;
-    var resval = document.getElementById(ResultValue).innerHTML = str(resval);
+    var resval = document.getElementById("ResultValue").innerHTML = str(resval);
 }
 
 document.getElementById("DivideButton").onclick = function() {
     if (secval != 0) {
         var resval = firstval / secval;
-        var resval = document.getElementById(ResultValue).innerHTML = str(resval);} else {window.alert("I cannot divide by zero!")}
+        var resval = document.getElementById("ResultValue").innerHTML = str(resval);} else {window.alert("I cannot divide by zero!")}
 }
