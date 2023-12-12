05758e13f0e1e34ea7b85113cea12cd3ae3f4804
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-12 15:57:58 +0100

    fix

--- ch4.js
+++ ch4.js
@@ -3,23 +3,34 @@ let firstval;
 let secval;
 let resval;
 
+function capture(firstval,secval) {
+    var firstval = document.getElementById("FirstValue").value
+    firstval = Number(firstval)
+    var secval = document.getElementById("SecValue").value
+    firstval = Number(secval)
+}
+
 document.getElementById("PlusButton").onclick = function() {
-    var resval = firstval + secval;
-    var resval = document.getElementById("ResultValue").innerHTML = String(resval);
+    capture(firstval,secval)
+    var resval = Number(firstval + secval);
+    var resval = document.getElementById("ResultValue").innerHTML = resval;
 }
 
 document.getElementById("MinusButton").onclick = function() {
-    var resval = firstval - secval;
+    capture(firstval,secval)
+    var resval = Number(firstval - secval);
     var resval = document.getElementById("ResultValue").innerHTML = resval;
 }
 
 document.getElementById("MultiButton").onclick = function() {
-    var resval = firstval * secval;
+    capture(firstval,secval)
+    var resval = Number(firstval * secval);
     var resval = document.getElementById("ResultValue").innerHTML = resval;
 }
 
 document.getElementById("DivideButton").onclick = function() {
+    capture(firstval,secval)
     if (secval != 0) {
-        var resval = firstval / secval;
+        var resval = Number(firstval / secval);
         var resval = document.getElementById("ResultValue").innerHTML = resval;} else {window.alert("I cannot divide by zero!")}
 }
