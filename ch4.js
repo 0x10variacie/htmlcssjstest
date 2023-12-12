e7577ab19981b0da25653f9996ff2d6f8c81d8e9
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-12 15:50:55 +0100

    String

--- ch4.js
+++ ch4.js
@@ -5,21 +5,21 @@ let resval;
 
 document.getElementById("PlusButton").onclick = function() {
     var resval = firstval + secval;
-    var resval = document.getElementById("ResultValue").innerHTML = str(resval);
+    var resval = document.getElementById("ResultValue").innerHTML = String(resval);
 }
 
 document.getElementById("MinusButton").onclick = function() {
     var resval = firstval - secval;
-    var resval = document.getElementById("ResultValue").innerHTML = str(resval);
+    var resval = document.getElementById("ResultValue").innerHTML = String(resval);
 }
 
 document.getElementById("MultiButton").onclick = function() {
     var resval = firstval * secval;
-    var resval = document.getElementById("ResultValue").innerHTML = str(resval);
+    var resval = document.getElementById("ResultValue").innerHTML = String(resval);
 }
 
 document.getElementById("DivideButton").onclick = function() {
     if (secval != 0) {
         var resval = firstval / secval;
-        var resval = document.getElementById("ResultValue").innerHTML = str(resval);} else {window.alert("I cannot divide by zero!")}
+        var resval = document.getElementById("ResultValue").innerHTML = String(resval);} else {window.alert("I cannot divide by zero!")}
 }
