5c9127611cc69da8327b4ac99cfe378eb1291728
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-13 04:12:20 +0100

    upd

--- ch5.js
+++ ch5.js
@@ -1,16 +1,16 @@
 console.log("js initialized")
 
-document.getElementById("But1").onclick = function () {
+document.getElementById("But1").value = function () {
     console.log("button 1 clicked");
     window.alert("1");
 }
 
-document.getElementById("But2").onclick = function () {
+document.getElementById("But2").value = function () {
     console.log("button 2 clicked");
     window.alert("2");
 }
 
-document.getElementById("But3").onclick = function () {
+document.getElementById("But3").value = function () {
     console.log("button 3 clicked");
     window.alert("3");
 }
