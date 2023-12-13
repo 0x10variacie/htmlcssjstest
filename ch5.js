ffcde77b423c0e430f6eccbcddebe8ec5376795f
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-13 03:58:46 +0100

    upd + new chapter

---
+++ ch5.js
@@ -0,0 +1,16 @@
+console.log("js initialized")
+
+document.getElementById("But1").onclick = function () {
+    console.log("button 1 clicked");
+    window.alert("1");
+}
+
+document.getElementById("But2").onclick = function () {
+    console.log("button 2 clicked");
+    window.alert("2");
+}
+
+document.getElementById("But3").onclick = function () {
+    console.log("button 3 clicked");
+    window.alert("3");
+}
