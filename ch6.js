6b49f3b7465010e1cc028e16a018405968861b61
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-15 05:13:47 +0100

    upd

--- ch6.js
+++ ch6.js
@@ -1,12 +1,4 @@
 console.log("js initialized")
-let AudioChosen;
 
-document.getElementById("PlayButton").onclick = function() {
-    var fileInput = document.getElementById("PickButton");
-    var selectedFile = fileInput.files[0];
-    AudioChosen = document.getElementById("PickButton").value;
-    audioChosen = URL.createObjectURL(selectedFile);
-    document.getElementById("audioPlayer").src = audioChosen;
-    document.getElementById("audioPlayer").style.display = "none";
-}
+
 
